e8c7246337d19a685168cbc0d6251eca177b96d2
Ronie Bituin <ronieB03@gmail.com>
2020-03-06 13:19:06 +0800

    2020-03-06A

--- footer.php
+++ footer.php
@@ -2,7 +2,7 @@
       <footer class="sticky-footer bg-white">
         <div class="container my-auto">
           <div class="copyright text-center my-auto">
-            <span>Copyright &copy; Theme Builder 2020</span>
+            <span>Copyright &copy; ESS Marketing 2020</span>
           </div>
         </div>
       </footer>
